b24cc62824eeeabb7f8e3fe2d7c4e9cf3388ea01
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-06 01:26:14 -0400

    made variable, queryselector, eventListener

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+//variables
+let creation = document.querySelector(".creation");
+
+//functions
+
+
+//event listener
 document.addEventListener("DOMcontentLoaded", console.log("DOM is loaded"));
 
 
@@ -5,3 +12,13 @@ document.addEventListener("DOMcontentLoaded", console.log("DOM is loaded"));
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log(event);
 });
+
+creation.addEventListener("click", (event) => {
+    let btn = event.currentTarget;
+    console.log(btn);
+});
+
+
+//when user clicks on button to `create issue` an event is triggerd []
+// the event will then either go to a function and create form || create form right after event listener []
+// form will have input for issue name []
