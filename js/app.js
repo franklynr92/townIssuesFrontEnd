c9af8cef2c5708931e112351216f5d2c6feea70c
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-07 04:20:49 -0400

    changed function to innerhtml

--- js/app.js
+++ js/app.js
@@ -4,7 +4,21 @@ let btn = document.querySelector("#change");
 let divIssues = document.querySelector(".issues");
 
 //functions
-function changeBtn(btn) {
+
+function changeBtn(btn){
+  btn.innerText ="Name the Issue"
+  creation.innerHTML = `
+  <form id="issues-form">
+    <input type="text" id="issue-name"/>
+    <input type="textarea" id="issue-description"/>
+    <input type="textarea" id="issue-cross-street1"/>
+    <input type="textarea" id="issue-cross-street2"/>
+    <input type="date" id="issue-date"/>
+    <input type="submit" value="Add The Issue" />
+  </form>
+`
+}
+function changeBtns(btn) {
 btn.innerText = "Name the issue";
 let issueForm = document.createElement("form");
   issueForm.setAttribute("id", "Issue-Name");
@@ -13,23 +27,51 @@ let firstInput = document.createElement("input");
   firstInput.setAttribute("placeholder", "issue-name");
   firstInput.setAttribute("id", "issue-name");
   firstInput.setAttribute("name", "issue-name");
-// let submitInput = document.createElement("input");
-//   submitInput.setAttribute("type", "submit");
-//   submitInput.setAttribute("value", "Submit");
   issueForm.appendChild(firstInput);
-  // issueForm.appendChild(submitInput);
    creation.appendChild(issueForm);
-firstInput.addEventListener('keypress', function (e){
-  
-  if (e.key === 'Enter') {
+let descBtn = document.createElement("BUTTON");
+   descBtn.setAttribute("class", "btn btn-primary");
+    descBtn.innerText = "Description of the issue";
+let secondInput = document.createElement("textarea");
+  secondInput.setAttribute("type", "text-area")
+  secondInput.setAttribute("placeholder", "description");
+  secondInput.setAttribute("id", "desc-name");
+  secondInput.setAttribute("name", "desc-name");
+  issueForm.appendChild(secondInput);
+  issueForm.appendChild(descBtn)
+let locationBtn = document.createElement("BUTTON");
+  locationBtn.setAttribute("class", "btn btn-primary");
+    locationBtn.innerText = "Location of the issue";
+let thirdInput = document.createElement("textarea");
+    thirdInput.setAttribute("type", "text-area")
+    thirdInput.setAttribute("placeholder", " Enter cross street");
+    thirdInput.setAttribute("id", "cros-street");
+    thirdInput.setAttribute("name", "cros-street");
+    issueForm.appendChild(thirdInput);
+    issueForm.appendChild(locationBtn);
+    let dateBtn = document.createElement("BUTTON");
+  dateBtn.setAttribute("class", "btn btn-primary");
+    dateBtn.innerText = "Date of the issue";
+let fourthInput = document.createElement("textarea");
+    fourthInput.setAttribute("type", "text-area")
+    fourthInput.setAttribute("placeholder", " Enter cross street");
+    fourthInput.setAttribute("id", "cros-street");
+    fourthInput.setAttribute("name", "cros-street");
+    issueForm.appendChild(fourthInput);
+    issueForm.appendChild(dateBtn);
+firstInput.addEventListener('submit', function (){ 
+
     event.preventDefault();
-    addIssueName()}
-});  //issueForm.addEventListener('submit', function(){
+   debugger   
+  
+  //issueForm.addEventListener('submit', function(){
 //   event.preventDefault();
 //   getIssueDescription();
 // });
 
-};
+});
+}
+
 
 function addIssueName(){
    let newIssueName = document.getElementById("issue-name");
@@ -38,7 +80,7 @@ function addIssueName(){
    let newIssueNameNode = document.createTextNode(newIssueName.value);
    newLi.appendChild(newIssueNameNode);
     let newUl = document.createElement("ul");
-    newUl.setAttribute("id", "issues")
+    newUl.setAttribute(`id`, `${newIssueNameNode}`);
     divIssues.appendChild(newUl);
     newUl.append(newLi);
 let descBtn = document.createElement("BUTTON");
@@ -51,12 +93,10 @@ let secondInput = document.createElement("textarea");
   secondInput.setAttribute("placeholder", "description");
   secondInput.setAttribute("id", "desc-name");
   secondInput.setAttribute("name", "desc-name");
-let submitInput = document.createElement("input");
-  submitInput.setAttribute("type", "submit");
-  submitInput.setAttribute("value", "Submit");
   descForm.appendChild(secondInput);
   descForm.appendChild(descBtn)
   creation.appendChild(descForm);
+  
 
 descBtn.addEventListener("click",  () => {
   
@@ -68,17 +108,29 @@ descBtn.addEventListener("click",  () => {
 
 function makeIssueDescription (){
   event.preventDefault();
-  btn.removeAttribute("id", "change");
-  btn.setAttribute("id", "changes");
-  let newIssueName = document.getElementById("desc-name");
+  let newIssueDesc = document.getElementById("desc-name");
   let newLi = document.createElement("li");
-  let newIssueNameNode = document.createTextNode(newIssueName.value);
+  let newIssueDescNode = document.createTextNode(newIssueDesc.value);
   newLi.innerText += "Description: ";
-  newLi.appendChild(newIssueNameNode);
+  newLi.appendChild(newIssueDescNode);
    let ul = document.getElementById("issues");
    ul.append(newLi);
+   
+    locationForm.appendChild(thirdInput);
+  let locationForm = document.createElement("form");
+        locationForm.setAttribute("id", "location-Name");
+  locationForm.appendChild(locationBtn)
+  creation.appendChild(locationForm);
+
+locationBtn.addEventListener("click",  () => {
+  event.preventDefault();
+    makeIssueLocation()
+});
 };
 
+function makeIssueLocation(){
+
+};
 //event listener
 document.addEventListener("DOMcontentLoaded", console.log );
 
